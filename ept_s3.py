5ea0632485f3e8d3489154e249515eb5928fcceb
x42bn6 <x42bn6@gmail.com>
2024-11-09 14:39:31 +0000

    UB can't finish last in playoffs

--- ept_s3.py
+++ ept_s3.py
@@ -141,6 +141,8 @@ def main():
     dreamleague_season_24.team_can_finish_between_gs2("Xtreme Gaming", 7, 7)
     dreamleague_season_24.team_can_finish_between_gs2("Team Waska", 8, 8)
 
+    dreamleague_season_24.team_can_finish_between("BetBoom Team", 1, 3)
+    dreamleague_season_24.team_can_finish_between("Team Spirit", 1, 3)
     dreamleague_season_24.guaranteed_playoff_lb_or_eliminated("Team Falcons", "PARIVISION")
 
     ept: EPT = EPT(
